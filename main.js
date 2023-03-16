3301e564995f60bf018ab6cf42353c5b407908b3
Monaliisa <mmss@cin.ufpe.br>
2023-03-16 12:23:55 -0300

    utilizando listas

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+    document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
