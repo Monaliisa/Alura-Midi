b43ccebdb5986eb0335b5568a64d13ec257fe221
Monaliisa <mmss@cin.ufpe.br>
2023-03-20 14:16:21 -0300

    condicoes no codigo

--- main.js
+++ main.js
@@ -14,8 +14,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space ') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
     tecla.onkeyup = function () {
